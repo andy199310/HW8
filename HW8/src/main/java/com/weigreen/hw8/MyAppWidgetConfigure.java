a71c6c931be73c26a2921db6a7111151896743fe
green <andy19310@gmail.com>
2013-05-18 11:48:49 +0800

    add some .java(?)

---
+++ HW8/src/main/java/com/weigreen/hw8/MyAppWidgetConfigure.java
@@ -0,0 +1,13 @@
+package com.weigreen.hw8;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by green on 2013/5/18.
+ */
+public class MyAppWidgetConfigure extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ HW8/src/main/java/com/weigreen/hw8/MyAppWidgetProvider.java
@@ -0,0 +1,9 @@
+package com.weigreen.hw8;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by green on 2013/5/18.
+ */
+public class MyAppWidgetProvider extends AppWidgetProvider{
+}
